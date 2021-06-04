a387b608f99b543b6acc38dbb28dbfd7ced108a5
Zsone Olds <zolds403@gmail.com>
2021-06-04 16:35:51 -0400

    controlled search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
 //jshint esversion: 8 
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
-function App() {
+
+
+const App = () => {
+  const [word, setWord] = useState('');
+  
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  };
+
+
   return (
-    <div className="App">
-     <Header title="Images Gallery"/>
-     <Search />
+    <div>
+      <Header title="Images Gallery"/>
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -2,15 +2,21 @@
 import React from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
-const Search = () => {
+const Search = ( { word, setWord, handleSubmit } ) => {
     return (
       <Container className="mt-4">
         <Row className="justify-content">
           <Col xs={12} md={8} lg={6}>
-            <Form>
+            <Form onSubmit={ handleSubmit }>
               <Form.Row>
                 <Col xs={9}>
-                  <Form.Control placeholder="Search for new image..." />
+                  <Form.Control 
+                  type="text"
+                  value={word}
+                  onChange={(e) => setWord(e.target.value)}
+                  placeholder="Search for new image..." 
+
+                  />
                 </Col>
                 <Col>
                   <Button variant="primary" type="submit">Search</Button>
