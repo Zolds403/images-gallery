167b2e721aa8661cbbf4012d4e6f9adbfbcc23e0
Zsone Olds <zolds403@gmail.com>
2021-06-13 12:26:59 -0400

    Changed API Forntend

--- api/main.py
+++ api/main.py
@@ -2,18 +2,20 @@ import os
 import requests
 from flask import Flask, request
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 load_dotenv(dotenv_path="./.env.local")
 
 UNSPLASH_URL = 'https://api.unsplash.com/photos/random'
 UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
-DEBUG = bool(os.environ.get("DEBUG", True))
+DEBUG = os.environ.get("DEBUG", True)
 
 if not UNSPLASH_KEY:
     raise EnvironmentError(
         "Please create .env.local file and insert UNSPLASH_KEY there")
 
 app = Flask(__name__)
+CORS(app)
 
 app.config["DEBUG"] = DEBUG
 
